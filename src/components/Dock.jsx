22998614644093284fccce269603776ba572043a
rakesh <rakeshsangode97@gmail.com>
2026-01-04 19:26:25 +0530

    implement window opening and closing state

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import React, { useRef } from 'react';
 import { Tooltip } from 'react-tooltip';
+import useWindowStore from '#store/window';
 
 const Dock = () => {
+	const { openWindow, closeWindow, windows } = useWindowStore();
 	const dockRef = useRef(null);
 
 	useGSAP(() => {
@@ -59,7 +61,22 @@ const Dock = () => {
 		};
 	}, []);
 
-	const toggleApp = (app) => {};
+	const toggleApp = (app) => {
+		if (!app.canOpen) return;
+
+		const window = windows[app.id];
+		if (!window) {
+			console.error(`Window not found for app: ${app.id}`);
+			return;
+		}
+		if (window.isOpen) {
+			closeWindow(app.id);
+		} else {
+			openWindow(app.id);
+		}
+
+		console.log(windows);
+	};
 
 	return (
 		<section id="dock">
